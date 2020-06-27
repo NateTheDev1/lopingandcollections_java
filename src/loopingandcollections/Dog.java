c89bf5fe849b313a802bd6a4f78e6029332ab0b0
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-27 00:04:48 -0400

    Dog class getters and setters

---
+++ src/loopingandcollections/Dog.java
@@ -0,0 +1,45 @@
+package loopingandcollections;
+
+public class Dog 
+{
+    private String breed;
+    private int avgWeight;
+    private boolean apartment;
+
+    public Dog(String breed, int avgWeight, boolean apartment)
+    {
+        this.breed = breed;
+        this.avgWeight = avgWeight;
+        this.apartment = apartment;
+    }
+
+    public String getBreed()
+    {
+        return this.breed;
+    }
+
+    public int getAvgWeight()
+    {
+        return this.avgWeight;
+    }
+
+    public boolean isApartment()
+    {
+        return this.apartment;
+    }
+
+    public void setBreed(String breed)
+    {
+        this.breed = breed;
+    }
+
+    public void setAvgWeight(int avgWeight)
+    {
+        this.avgWeight = avgWeight;
+    }
+
+    public void setApartment(boolean apartment)
+    {
+        this.apartment = apartment;
+    }
+}
